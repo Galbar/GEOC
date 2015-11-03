7bd26cc4bd71c5d28f790e00ff93528d25cbe6c2
Daniel Martí <mvdan@mvdan.cc>
2015-11-03 21:48:57 +0100

    Remove badly done MAKE_ARGS

--- setup.sh
+++ setup.sh
@@ -5,13 +5,12 @@ set -e
 EXERCISES_DIR=exercises
 DEMOS_DIR=demos
 CGAL_DIR=CGAL-3.8
-MAKE_ARGS=$1
 
 function build {
 	for proj in `ls $1`; do
 		pushd $1/$proj
 		qmake >/dev/null || continue
-		make $MAKE_ARGS
+		make
 		popd
 	done
 }
@@ -21,11 +20,11 @@ echo "Setting up geoc viewer installation"
 echo "Compiling CGAL"
 pushd $CGAL_DIR
 cmake .
-make $1
+make
 popd
 
 echo "Making initial library build (release mode)"
-make $MAKE_ARGS
+make
 
 echo "Compiling exercises"
 build $EXERCISES_DIR
